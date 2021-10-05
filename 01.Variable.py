8f7e9c965877cbf5a6a8157f36d853db863d1c98
Ayesha Shaheen <ayeshashaheen542@gmail.com>
2021-10-05 22:45:45 +0500

    added a file

---
+++ 01.Variable.py
@@ -0,0 +1,20 @@
+'''printing the variables'''
+#  in string,we can write with three diff.ways:(1)'A' (2)"A" (3)'''A'''
+a= '''Ayesha'''
+b= 84203
+c= 7321.32
+'''in booleans, we just use Falsa,True and Not word'''
+e= False
+d= None
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+# printing the types of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
